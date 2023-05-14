a639eaaf3f14f7e76581e1ab51ec6c118c665f62
FV250064 <Fernando.Valls@ncr.com>
2023-05-14 19:57:18 -0300

    modelando vistas + navbar lateral

---
+++ app/src/main/java/com/example/projectFinal/activities/ui/organization/OrganizationFragment.kt
@@ -0,0 +1,31 @@
+package com.example.projectFinal.activities.ui.organization
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.example.projectFinal.R
+import com.example.projectFinal.databinding.FragmentHomeBinding
+
+class OrganizationFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
+
+}
--- app/src/main/java/com/example/projectFinal/fragments/Fragment_login.kt
+++ app/src/main/java/com/example/projectFinal/fragments/Fragment_login.kt
@@ -38,40 +38,40 @@ class Fragment_login : Fragment() {
         val buttonRegister = view.findViewById<Button>(R.id.loginRegister)
         val userName = view.findViewById<EditText>(R.id.username)
 
-        buttonLogin.setOnClickListener {
-
-            parentFragmentManager.commit {
-                replace<Fragment_edit_customer>(R.id.fragmentContainerView)
-                setReorderingAllowed(true)
-//                disallowAddToBackStack()
-            }
-
-            if (userName.text.toString() == "fer"){
-                Toast.makeText(
-                    requireContext(),
-                    "true",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }else{
-                Toast.makeText(
-                    requireContext(),
-                    "false",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-
-
-        }
-
-        buttonRegister.setOnClickListener {
-            Toast.makeText(
-                requireContext(),
-                "Probando boton REGISTER!",
-                Toast.LENGTH_SHORT
-            ).show()
-        }
-
-
+//        buttonLogin.setOnClickListener {
+//
+//            parentFragmentManager.commit {
+//                replace<Fragment_admin>(R.id.fragmentContainerView)
+//                setReorderingAllowed(true)
+////                disallowAddToBackStack()
+//            }
+//
+//            if (userName.text.toString() == "fer"){
+//                Toast.makeText(
+//                    requireContext(),
+//                    "true",
+//                    Toast.LENGTH_SHORT
+//                ).show()
+//            }else{
+//                Toast.makeText(
+//                    requireContext(),
+//                    "false",
+//                    Toast.LENGTH_SHORT
+//                ).show()
+//            }
+//
+//
+//        }
+//
+//        buttonRegister.setOnClickListener {
+//            Toast.makeText(
+//                requireContext(),
+//                "Probando boton REGISTER!",
+//                Toast.LENGTH_SHORT
+//            ).show()
+//        }
+//
+//
 
     }
 
