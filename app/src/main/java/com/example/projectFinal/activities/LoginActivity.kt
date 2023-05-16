1ae4807151c055abe64cfce7ed0a67beab50b0c0
mosheFuks <moshifuks2001@gmail.com>
2023-05-16 09:19:49 -0300

    Funcion agregada en LoginActivity

--- app/src/main/java/com/example/projectFinal/activities/LoginActivity.kt
+++ app/src/main/java/com/example/projectFinal/activities/LoginActivity.kt
@@ -11,12 +11,19 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import com.example.projectFinal.R
 import com.example.projectFinal.databinding.ActivityMainBinding
 import com.example.projectFinal.databinding.ActivityNavBinding
+import com.example.projectFinal.fragments.Fragment_edit_customer
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.biometric.BiometricManager.Authenticators.DEVICE_CREDENTIAL
+import androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG
+import androidx.biometric.BiometricManager
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -45,6 +52,41 @@ class LoginActivity : AppCompatActivity() {
                 "Probando boton REGISTER!",
                 Toast.LENGTH_SHORT
             ).show()
+        }
+
+        val fingerPrint = findViewById<TextView>(R.id.loginBiometricButton);
+        //val context = activity?.applicationContext
+
+        var executor = ContextCompat.getMainExecutor(this!!)
+        var canAuthenticate = false
+
+        fun moveToEditCustomerFragment() {
+            val intent =
+                Intent(applicationContext, NavActivity::class.java)
+            startActivity(intent)
+            finish()
+            Toast.makeText(
+                this,
+                "Login succeeded with finger print",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        var biometricPrompt = androidx.biometric.BiometricPrompt(this, ContextCompat.getMainExecutor(this!!), object: androidx.biometric.BiometricPrompt.AuthenticationCallback(){
+            override fun onAuthenticationSucceeded(result: androidx.biometric.BiometricPrompt.AuthenticationResult) {
+                super.onAuthenticationSucceeded(result)
+                moveToEditCustomerFragment()
+            }
+        })
+
+        var promptInfo = androidx.biometric.BiometricPrompt.PromptInfo.Builder()
+            .setTitle("Biometric Authentication")
+            .setSubtitle("Login using your finger print")
+            .setNegativeButtonText("Cancel")
+            .build()
+
+        fingerPrint.setOnClickListener {
+            biometricPrompt.authenticate(promptInfo)
         }
     }
 
